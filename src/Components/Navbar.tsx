e46b64d10d22daa6e2a048141afedfb28aa0c226
carson <carsonfrankovich6@gmail.com>
2023-05-06 22:37:43 -0400

    finish opening

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,14 +1,39 @@
 import logo from "../assets/logo.png";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [isNavVisible, setIsNavVisible] = useState(true);
+  const [userScrolledUp, setUserScrolledUp] = useState(false);
+  let currentScrollPosition = window.scrollY;
+
+  useEffect(() => {
+    window.addEventListener("scroll", scrollFunc);
+    return () => {
+      window.removeEventListener("scroll", scrollFunc);
+    };
+  });
+
+  const scrollFunc = () => {
+    const scrolling = window.scrollY < currentScrollPosition;
+    setUserScrolledUp(scrolling);
+
+    currentScrollPosition = window.scrollY;
+    const visible = currentScrollPosition < 100;
+    setIsNavVisible(visible);
+  };
+
   return (
-    <nav>
+    <nav
+      className={`${isNavVisible ? "" : "navBarScrolled"} ${
+        userScrolledUp ? "navBarBlackBG" : ""
+      }`}
+    >
       <img src={logo} className="navBarLogo"></img>
       <div className="navBarLinks">
+        <a href="index">Home</a>
         <a href="index">About</a>
         <a href="index">Blog</a>
         <a href="index">Products</a>
-        <a href="index">Services</a>
       </div>
       <button className="navBarButton">Contact Us</button>
     </nav>
--- src/Components/Opening.tsx
+++ src/Components/Opening.tsx
@@ -1,5 +1,10 @@
 const Opening = () => {
-  return <div className="opening"></div>;
+  return (
+    <div className="opening">
+      <h1>Software Development Tailored to Unique Needs</h1>
+      <a href="#">see how technology can bring ideas to life ↴</a>
+    </div>
+  );
 };
 
 export default Opening;
