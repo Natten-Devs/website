913eb42cb5be3b10d5efac1b787783537ea566ee
carson <carsonfrankovich6@gmail.com>
2023-06-03 17:41:02 -0400

    updating router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,4 @@
-import {
-  HashRouter,
-  Route,
-  Routes,
-} from "../node_modules/react-router-dom/dist/index";
+import { Route, Routes } from "../node_modules/react-router-dom/dist/index";
 
 import Home from "./Home";
 import Blog from "./Blog";
@@ -18,32 +14,30 @@ import CAS_DescriptionPage from "./Components/Careers/CAS_DescriptionPage";
 
 const App = () => {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="*" element={<NotFound />}></Route>
-        <Route path="/" element={<Home />}></Route>
-        <Route path="/home" element={<Home />}></Route>
-        <Route path="/about" element={<About />}></Route>
-        <Route path="/services" element={<Services />}></Route>
-        <Route path="/blog" element={<Blog />}></Route>
-        <Route path="/careers" element={<Careers />}></Route>
-        <Route path="/legal" element={<Legal />}></Route>
-        <Route path="/contact" element={<Contact />}></Route>
+    <Routes>
+      <Route path="*" element={<NotFound />}></Route>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/home" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+      <Route path="/services" element={<Services />}></Route>
+      <Route path="/blog" element={<Blog />}></Route>
+      <Route path="/careers" element={<Careers />}></Route>
+      <Route path="/legal" element={<Legal />}></Route>
+      <Route path="/contact" element={<Contact />}></Route>
 
-        <Route
-          path="/careers/market-analyst"
-          element={<MA_DescriptionPage />}
-        ></Route>
-        <Route
-          path="/careers/full-stack-engineer"
-          element={<FSE_DescriptionPage />}
-        ></Route>
-        <Route
-          path="/careers/client-acquisition-specialist"
-          element={<CAS_DescriptionPage />}
-        ></Route>
-      </Routes>
-    </HashRouter>
+      <Route
+        path="/careers/market-analyst"
+        element={<MA_DescriptionPage />}
+      ></Route>
+      <Route
+        path="/careers/full-stack-engineer"
+        element={<FSE_DescriptionPage />}
+      ></Route>
+      <Route
+        path="/careers/client-acquisition-specialist"
+        element={<CAS_DescriptionPage />}
+      ></Route>
+    </Routes>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./css/style.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <React.StrictMode>
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>
 );
